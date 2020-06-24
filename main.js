92d650c81a04a9c62d36a3cffe3c4c7d7f85d163
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-24 11:55:25 -0400

    changes

---
+++ main.js
@@ -0,0 +1,10 @@
+function challenge(){
+    let num = 1;
+    while(num <= 5) {
+        console.log('challengeBanner');
+        num = num + 1;
+    }
+}
+
+challengeBanner(1) // -> '##### Challenge 1 #####'
+challenge();
